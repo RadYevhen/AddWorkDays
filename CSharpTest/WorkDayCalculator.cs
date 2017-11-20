89d29f35739e3e2d9f13b0f2197dabbadf913e5f
RadYevgeniy <rad.yevhen@gmail.com>
2017-11-20 16:01:05 +0200

    V1

---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {   
+        // старая(медленная) версия
+        /*public Boolean IsWeekDays(DateTime startDate, WeekEnd[] weekEnds)
+        {
+            bool isWeekDay = true;
+
+            for (int i = 0; i < weekEnds.Length; i++)
+            {
+                if (startDate >= weekEnds[i].StartDate &&
+                    startDate <= weekEnds[i].EndDate)
+                {
+                    isWeekDay = false;
+                }
+            }
+            return isWeekDay;
+        }*/
+        public int IndexBinarySearch(DateTime startDate, WeekEnd[] weekEnds)
+        {
+            int index = 0;
+            int a = 0; // начало отрезка
+            int b = weekEnds.Length - 1; // конец отрезка
+            
+            // пока длина отрезка [a,b] > 1 рубим его делением пополам
+            while (b - a > 1)
+            {
+                index = (a + b) / 2; // целочисленное деление на 2
+
+                if (startDate <= weekEnds[index].EndDate)
+                {
+                    // обрубаем правую часть отрезка [a, b]
+                    b = index;
+                }
+                else if (startDate > weekEnds[index].EndDate)
+                {
+                    // обрубаем левую часть отрезка [a, b]
+                    a = index;
+                }
+            }
+
+            return index;
+        }
+
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            //throw new NotImplementedException();
+            //int count = 0;
+            int day = 1;
+            int lenght = 0;
+            DateTime returnDate = startDate;
+            try
+            {
+                lenght = weekEnds.Length;
+            } catch { }
+
+            if (lenght == 0)
+            {
+                returnDate = startDate.AddDays(dayCount);
+            }
+            else
+            {
+                weekEnds = weekEnds.OrderBy(a => a.StartDate).ToArray(); // сортируем массив по начальной дате
+
+                // находим стартовую позицию в массиве
+                int index = IndexBinarySearch(startDate, weekEnds);
+
+                // если текущий день - выходной, то переставить на ближайший рабочий
+                if (returnDate >= weekEnds[index].StartDate)
+                {
+                    returnDate = weekEnds[index].EndDate.AddDays(day);
+                    ++index;
+                }
+
+                dayCount = dayCount - 1; // dayCount-1 - потому что не считаем начальную(стартовую) дату
+                while (dayCount > 0) 
+                {
+                    // если мы за пределами массива(след. выходных уже нет, закончились элементы) ->
+                    // добавляем столько сколько дней
+                    if (index > weekEnds.Length - 1)
+                    {
+                        returnDate = returnDate.AddDays(dayCount);
+                        break;
+                    }
+                    // сколько рабочих дней до след. выходного
+                    int daysToNextWeekends = (weekEnds[index].StartDate - returnDate).Days;
+
+                    // если кол-во дней которые надо добавить не достают до след. выходных - добавляем эти дни
+                    if (dayCount < daysToNextWeekends)
+                    {
+                        returnDate = returnDate.AddDays(dayCount);
+                        break;
+                    }
+                    // в противном случаи переводим дату на след. день после след. выходных ->
+                    // -> и уменьшаем кол-во дней которые надо добавить
+                    else
+                    {
+                        returnDate = weekEnds[index].EndDate.AddDays(day);
+                        ++index;
+                        dayCount -= daysToNextWeekends;
+                    }
+       
+                }
+
+                // старая(медленная) версия
+                /* while (count < dayCount )
+                 {
+                     bool isWeekDay = IsWeekDays(startDate, weekEnds);
+                     if ( isWeekDay == true)
+                     {
+                         returnDate = startDate;
+                         count++;
+                         if (count == dayCount) { break; }
+                     }
+                     startDate = startDate.AddDays(day);
+
+                 }*/
+            }
+
+            return returnDate;
+        }
+    }
+}
---
+++ CSharpTest/WorkDayCalculatorTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    [TestClass]
+    public class WorkDayCalculatorTests
+    {
+
+        [TestMethod]
+        public void TestNoWeekEnd()
+        {
+            DateTime startDate = new DateTime(2014, 12, 1);
+            int count = 10;
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, null);
+
+            Assert.AreEqual(startDate.AddDays(count), result);
+
+        }
+
+        [TestMethod]
+        public void TestNormalPath()
+        {
+            DateTime startDate = new DateTime(2017, 4, 21);
+            int count = 5;
+            WeekEnd[] weekends = new WeekEnd[1]
+            {
+                new WeekEnd(new DateTime(2017, 4, 23), new DateTime(2017, 4, 25))
+            }; 
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2017, 4, 28)));
+        }
+
+        [TestMethod]
+        public void TestWeekendAfterEnd()
+        {
+            DateTime startDate = new DateTime(2017, 4, 21);
+            int count = 5;
+            WeekEnd[] weekends = new WeekEnd[2]
+            {
+                new WeekEnd(new DateTime(2017, 4, 23), new DateTime(2017, 4, 25)),
+                new WeekEnd(new DateTime(2017, 4, 29), new DateTime(2017, 4, 29))
+            };
+            
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2017, 4, 28)));
+        }
+
+        [TestMethod]
+        public void TestNew1()
+        {
+            DateTime startDate = new DateTime(2017, 11, 13);
+            int count = 11;
+            WeekEnd[] weekends = new WeekEnd[4]
+            {
+                new WeekEnd(new DateTime(2017, 11, 11), new DateTime(2017, 11, 12)),
+                new WeekEnd(new DateTime(2017, 12, 02), new DateTime(2017, 12, 03)),
+                new WeekEnd(new DateTime(2017, 11, 25), new DateTime(2017, 11, 26)),
+                new WeekEnd(new DateTime(2017, 11, 18), new DateTime(2017, 11, 19))
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2017, 11, 27)));
+        }
+
+        [TestMethod]
+        public void TestNew2()
+        {
+            DateTime startDate = new DateTime(2017, 11, 14);
+            int count = 6;
+            WeekEnd[] weekends = new WeekEnd[2]
+            {
+                new WeekEnd(new DateTime(2017, 11, 21), new DateTime(2017, 11, 21)),
+                new WeekEnd(new DateTime(2017, 11, 17), new DateTime(2017, 11, 19))
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2017, 11, 23)));
+        }
+    }
+}
